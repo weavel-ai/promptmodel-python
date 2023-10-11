a8e4c8c2d777c83c232d1464443c5128b87ed1ec
EngineerA <engineerA314@github.com>
2023-10-11 18:33:23 +0900

    FIX: fix error in signal handler

--- fastllm/cli/signal_handler.py
+++ fastllm/cli/signal_handler.py
@@ -1,7 +1,9 @@
 import signal
 import sys
-from fastllm.utils.config_utils import upsert_config
+from fastllm.utils.config_utils import upsert_config, read_config
 
 def dev_terminate_signal_handler(sig, frame):
-    upsert_config({"online": False}, section="dev_branch")
+    config = read_config()
+    if "dev_branch" in config:
+        upsert_config({"online": False}, section="dev_branch")
     sys.exit(0)  
