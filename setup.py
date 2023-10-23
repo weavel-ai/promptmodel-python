6f6b4a2e92d05eead405639a1b0b5a8ae807c58f
Sounho Chung <aschung01@snu.ac.kr>
2023-10-23 21:08:47 +0900

    CHG : Updated pypi version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="promptmodel",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
