82a68e653793dffc60cd81979e9d444aed12a9c4
EngineerA <engineerA314@github.com>
2023-11-30 17:41:24 +0900

    UPDATE: update version 0.1.0

--- promptmodel/__init__.py
+++ promptmodel/__init__.py
@@ -3,4 +3,4 @@ from .prompt_model import PromptModel
 from .chat_model import ChatModel
 from .promptmodel_init import init
 
-__version__ = "0.0.31"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="promptmodel",
-    version="0.0.31",
+    version="0.1.0",
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
