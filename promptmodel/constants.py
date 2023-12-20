0f54e76d0f4ad14910ea6ec176dad16bb1725c24
EngineerA <engineerA314@github.com>
2023-12-20 20:19:02 +0900

    fix: fix constants.py to use .env

--- promptmodel/constants.py
+++ promptmodel/constants.py
@@ -3,23 +3,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 testmode: str = os.environ.get("TESTMODE", "false")
+
 if testmode == "true":
-    # ENDPOINT_URL = "https://2451-124-49-14-184.ngrok-free.app/api/cli"
-    ENDPOINT_URL = "https://weavel.serveo.net/api/cli"
-    # ENDPOINT_URL = "https://promptmodel-colab.up.railway.app/api/cli"
-    # ENDPOINT_URL = "http://localhost:8443/api/cli"
-
-    # WEB_CLIENT_URL = "http://localhost:3000"
-    WEB_CLIENT_URL = "https://promptmodel.serveo.net"
-    # WEB_CLIENT_URL = "https://app.promptmodel.run"
-    # GRANT_ACCESS_URL = "http://localhost:3000/cli/grant-access"
-    GRANT_ACCESS_URL = "https://promptmodel.serveo.net/cli/grant-access"
-    # GRANT_ACCESS_URL = "https://app.promptmodel.run/cli/grant-access"
+    ENDPOINT_URL = (
+        os.environ.get(
+            "TESTMODE_PROMPTMODEL_BACKEND_PUBLIC_URL", "http://localhost:8000"
+        )
+        + "/api/cli"
+    )
+    WEB_CLIENT_URL = os.environ.get(
+        "TESTMODE_PROMPTMODEL_FRONTEND_PUBLIC_URL", "http://localhost:3000"
+    )
+    GRANT_ACCESS_URL = WEB_CLIENT_URL + "/cli/grant-access"
 else:
-    ENDPOINT_URL = "https://promptmodel.up.railway.app/api/cli"
-    WEB_CLIENT_URL = "https://app.promptmodel.run"
-
-    GRANT_ACCESS_URL = "https://app.promptmodel.run/cli/grant-access"
+    ENDPOINT_URL = (
+        os.environ.get("PROMPTMODEL_BACKEND_PUBLIC_URL", "http://localhost:8000")
+        + "/api/cli"
+    )
+    WEB_CLIENT_URL = os.environ.get(
+        "PROMPTMODEL_FRONTEND_PUBLIC_URL", "http://localhost:3000"
+    )
+    GRANT_ACCESS_URL = WEB_CLIENT_URL + "/cli/grant-access"
 
 PROMPTMODEL_DEV_FILENAME = os.path.join(os.getcwd(), "promptmodel_dev.py")
 PROMPTMODEL_DEV_STARTER_FILENAME = "STARTER.py"
