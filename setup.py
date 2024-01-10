8f991bde090c0accc8be5df7064b143e3127257b
engineerA314 <engineerA314@github.com>
2024-01-10 15:13:22 +0900

    UPDATE: update version 0.1.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="promptmodel",
-    version="0.1.13",
+    version="0.1.14",
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
